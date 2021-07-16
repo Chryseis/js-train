fd17c43c40d0372f6b99ac613cc7c687e6153e6a
allen <294931812@qq.com>
2021-07-16 18:14:28 +0800

    refactor: add some code

---
+++ src/promise/promiseAll.js
@@ -0,0 +1,35 @@
+function promiseAll(arr) {
+  let retVal = []
+  if (arr && !arr.length) {
+    console.warn('参数有问题')
+  } else {
+    return new Promise(resolve => {
+      arr.forEach(promise => {
+        if (!(promise instanceof Promise)) {
+          promise = Promise.resolve(promise)
+        }
+
+        promise.then(data => {
+          retVal.push(data)
+          if (retVal.length === arr.length) {
+            resolve(retVal)
+          }
+        })
+      })
+    })
+  }
+}
+
+const promise1 = () => Promise.resolve(1)
+const promise2 = () => Promise.resolve(2)
+const promise3 = () => Promise.resolve(3)
+const promise4 = () => Promise.resolve(4)
+const promise5 = () => Promise.resolve(5)
+
+Promise.all([promise1(), promise2(), promise3(), promise4(), promise5()]).then(data => {
+  console.log(data)
+})
+
+promiseAll([promise1(), promise2(), promise3(), promise4(), promise5()]).then(data => {
+  console.log(data)
+})
---
+++ src/promise/promiseRace.js
@@ -0,0 +1,1 @@
+function promiseRace(arr) {}
