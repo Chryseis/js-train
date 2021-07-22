fcc658a53f6523569327272c67ccbdca1dfee5eb
chrys <294931812@qq.com>
2021-07-23 02:21:23 +0800

    refactor: add JsBridge

---
+++ src/jsBridge/index.js
@@ -0,0 +1,51 @@
+;(function () {
+  const callbacks = {}
+
+  // 如果使用iframe传值
+  function renderIframe(url) {
+    try {
+      let iframeElem = document.createElement('iframe')
+      iframeElem.setAttribute('src', url)
+      iframeElem.setAttribute('style', 'display:none;')
+      iframeElem.setAttribute('height', '0px')
+      iframeElem.setAttribute('width', '0px')
+      iframeElem.setAttribute('frameborder', '0')
+      document.body.appendChild(iframeElem)
+      setTimeout(() => {
+        document.body.removeChild(iframeElem)
+        iframeElem = null
+      }, 300)
+    } catch (e) {}
+  }
+
+  window.JSBridge = {
+    dispatch(name, data) {
+      const event = document.createEvent('Events')
+      event.initEvent(name, false, true)
+      event.data = data
+      document.dispatchEvent(event)
+    },
+
+    invoke(bridgeName, data, callback) {
+      const callbackId = `${name}_${Math.floor(Math.random() * new Date().getTime())}`
+      callbacks[callbackId] = callback
+      window.postBridgeMessage({
+        bridgeName,
+        data,
+        callbackId
+      })
+    },
+
+    receiveMessage(msg) {
+      const { data, callbackId } = msg
+      if (callbacks[callbackId]) {
+        callbacks[callbackId](data)
+        delete callbacks.callbackId
+      }
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    window.JSBridge.dispatch('myJSBridgeReady')
+  })
+})()
