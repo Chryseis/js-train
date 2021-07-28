a8bafb46cc2362de75d7cf9d75d9f2dcba6d7b64
allen <294931812@qq.com>
2021-07-28 15:41:58 +0800

    refactor: add event model

---
+++ src/event/index.js
@@ -0,0 +1,79 @@
+class Event {
+  events
+  constructor(initEvents = {}) {
+    this.events = initEvents
+  }
+
+  on(event, fn) {
+    if (Array.isArray(event)) {
+      for (let i = 0, l = event.length; i < l; i++) {
+        this.on(event[i], fn)
+      }
+    } else {
+      this.events[event] = (this.events[event] || []).concat(fn)
+    }
+    return this
+  }
+
+  emit(event) {
+    let cbs = this.events[event]
+    const args = Array.from(arguments).slice(1)
+    if (cbs) {
+      cbs.forEach(function (cb) {
+        cb.apply(this, args)
+      })
+    }
+
+    return this
+  }
+
+  off(event, fn) {
+    if (Array.isArray(event)) {
+      for (let i = 0, l = event.length; i < l; i++) {
+        this.off(event[i], fn)
+      }
+    } else {
+      let cbs = this.events[event]
+      if (!fn) {
+        this.events[event] = null
+        return this
+      }
+
+      if (cbs) {
+        this.events[event] = cbs.filter(cb => cb !== fn)
+      }
+    }
+
+    return this
+  }
+
+  once(event, fn) {
+    const on = () => {
+      this.off(event, on)
+      fn.apply(this, arguments)
+    }
+
+    this.on(event, on)
+
+    return this
+  }
+}
+
+const event = new Event()
+
+const fn = () => {
+  console.log('I click')
+}
+
+event.on('click', fn)
+
+event.once('touch', function () {
+  console.log('I touch')
+})
+
+event.emit('click')
+event.off('click', fn)
+event.emit('click')
+
+event.emit('touch')
+event.emit('touch')
