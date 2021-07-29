e9481415eab30e2205dc5ca703417956dac1b59d
allen <294931812@qq.com>
2021-07-29 16:42:57 +0800

    feat: add number format

---
+++ src/luckyDraw/index.js
@@ -0,0 +1,19 @@
+/*
+请实现抽奖函数rand，保证随机性
+输入为表示对象数组，对象有属性n表示人名，w表示权重
+随机返回一个中奖人名，中奖概率和w成正比
+*/
+let peoples = [
+  { n: 'p1', w: 100 },
+  { n: 'p2', w: 200 },
+  { n: 'p3', w: 100 }
+]
+let rand = function (p) {
+  const ret = p.map(o => ({ ...o, score: o.w * Math.random() }))
+
+  const max = Math.max(...ret.map(o => o.score))
+
+  return ret.find(o => o.score === max).n
+}
+
+console.log(rand(peoples))
