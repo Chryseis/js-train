ee5a3c45e45cd0be376e92af1d2b9e4fc3b9c07a
chrys <294931812@qq.com>
2021-08-01 11:51:49 +0800

    feature: add tree code

--- src/luckyDraw/index.js
+++ src/luckyDraw/index.js
@@ -3,11 +3,18 @@
 输入为表示对象数组，对象有属性n表示人名，w表示权重
 随机返回一个中奖人名，中奖概率和w成正比
 */
-let peoples = [
-  { n: 'p1', w: 100 },
-  { n: 'p2', w: 200 },
-  { n: 'p3', w: 100 }
-]
+const genPeoples = count => {
+  let arr = []
+  for (let i = 0; i < count; i++) {
+    arr.push({ n: `p${i + 1}`, w: Math.floor(Math.random() * 5000000) })
+  }
+
+  return arr
+}
+
+const peoples = genPeoples(200)
+
+// 方法1
 const rand = function (p) {
   const ret = p.map(o => ({ ...o, score: o.w * Math.random() }))
 
@@ -16,4 +23,24 @@ const rand = function (p) {
   return ret.find(o => o.score === max).n
 }
 
+let startTime = +new Date()
 console.log(rand(peoples))
+console.log(+new Date() - startTime, 'rand1')
+
+// 方法2
+const rand2 = function (p) {
+  const ret = p.reduce((arr, o) => {
+    const multiple = Math.floor(o.w / 100)
+    for (let i = 0; i < multiple; i++) {
+      arr.push(o)
+    }
+    return arr
+  }, [])
+
+  const randomIdx = Math.floor(ret.length * Math.random())
+  return ret[randomIdx]
+}
+
+let startTime1 = +new Date()
+console.log(rand2(peoples).n)
+console.log(+new Date() - startTime1, 'rand2')
