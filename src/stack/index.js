2e8c87c94782206cbf60cc61b21417e230e5c19b
allen <294931812@qq.com>
2021-08-02 11:51:59 +0800

    feat: add stack code

--- src/stack/index.js
+++ src/stack/index.js
@@ -2,15 +2,31 @@
 class Stack {
   items = []
 
-  push(item) {}
+  // 压栈
+  push(item) {
+    this.items.push(item)
+  }
 
-  pop() {}
+  // 出栈
+  pop() {
+    return this.items.pop()
+  }
 
-  peek() {}
+  // 查看栈顶元素
+  peek() {
+    return this.items[this.items.length - 1]
+  }
 
-  isEmpty() {}
+  // 判断栈是否为空
+  isEmpty() {
+    return this.items.length === 0
+  }
 
-  size() {}
+  // 获取栈中元素的个数
+  size() {
+    return this.items.length
+  }
 
+  // 以字符串形式输出栈内元素
   toString() {}
 }
