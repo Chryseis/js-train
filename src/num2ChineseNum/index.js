dd1642866a7df3db21e0a55d20a297888bbd541b
allen <294931812@qq.com>
2022-09-13 16:52:06 +0800

    wip: edit num2chineseNum

--- src/num2ChineseNum/index.js
+++ src/num2ChineseNum/index.js
@@ -5,12 +5,27 @@ function num2ChineseNum(num) {
 
   const chineseNum = { 0: '零', 1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六', 7: '七', 8: '八', 9: '九' }
 
-  const chineseUnit = { 0: '', 1: '十', 2: '百', 3: '千', 4: '万', 5: '十万', 6: '百万', 7: '千万', 8: '亿' }
+  const chineseUnit = {
+    0: '',
+    1: '十',
+    2: '百',
+    3: '千',
+    4: '万',
+    5: '十万',
+    6: '百万',
+    7: '千万',
+    8: '亿',
+    9: '十亿',
+    10: '百亿',
+    11: '亿亿'
+  }
 
   let retNum = ''
 
   for (let i = 1; i <= length; i++) {
-    const divisor = Math.pow(10, length - i)
+    let exponent = length - i
+
+    const divisor = Math.pow(10, exponent)
 
     const result = Math.floor(num / divisor)
 
@@ -22,19 +37,21 @@ function num2ChineseNum(num) {
           retNum = retNum + chineseNum[result]
         }
       } else {
-        if (remainder / Math.pow(10, 4) > 0) {
-          retNum = retNum + chineseNum[result] + chineseUnit[length - i].replace('万', '')
+        if (remainder > Math.pow(10, 8)) {
+          retNum = retNum + chineseNum[result] + chineseUnit[exponent].replace(/亿/, '')
+        } else if (remainder > Math.pow(10, 4)) {
+          retNum = retNum + chineseNum[result] + chineseUnit[exponent].replace(/万/, '')
         } else {
-          retNum = retNum + chineseNum[result] + chineseUnit[length - i]
+          retNum = retNum + chineseNum[result] + chineseUnit[exponent]
         }
       }
     } else {
-      if (result !== 0) {
-        retNum = retNum + chineseNum[result] + chineseUnit[length - i]
-      } else {
+      if (result === 0) {
         if (!retNum.endsWith('零')) {
           retNum = retNum + chineseNum[result]
         }
+      } else {
+        retNum = retNum + chineseNum[result] + chineseUnit[exponent]
       }
     }
 
