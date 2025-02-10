2c4ba28d4467c2d18ec5bd8782d117b98d746747
allen <294931812@qq.com>
2025-02-10 23:16:49 +0800

    feat(requestQueue): 实现用户信息请求队列
    - 添加 requestQueue 模块，用于管理用户信息请求
    - 实现 requestUserProfile函数，支持限流请求用户信息
    - 添加示例代码，展示如何使用请求队列功能

---
+++ src/requestQueue/index.js
@@ -0,0 +1,43 @@
+// requestUserProfile 是个通用查询用户接口
+// 传入uid 拿用户昵称 10个并发请求会阻塞接口，10个依次请求耗时久显示昵称太慢
+// 需求优化请求 在并发和耗时之间掌握一个平衡
+const requestProfile = uid => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ uid, nick: `nick-${uid}`, age: '18' })
+    }, 1000)
+  })
+}
+
+let requestQueue = []
+
+const requestUserProfile = (uid, max) => {
+  return new Promise(resolve => {
+    if (requestQueue.length <= max) {
+      requestQueue.push(uid)
+      requestProfile(uid)
+        .then(res => {
+          return res
+        })
+        .then(res => {
+          requestQueue = requestQueue.filter(item => item !== uid)
+          resolve(res)
+        })
+    } else {
+      setTimeout(() => resolve(requestUserProfile(uid, max)))
+    }
+  })
+}
+
+;(async function () {
+  try {
+    const result = await Promise.all([
+      requestUserProfile(1, 2),
+      requestUserProfile(2, 2),
+      requestUserProfile(3, 2),
+      requestUserProfile(4, 2),
+      requestUserProfile(5, 2)
+    ])
+    console.log(result)
+  } catch (err) {}
+})()
